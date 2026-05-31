e89071680219ebf2805030a3d9dfd686331b865d
ibrahimgermaine1-dev <ibrahimgermaine1@gmail.com>
2026-05-31 22:19:57 +0100

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  reactStrictMode: false,
-};
-
-export default nextConfig;
