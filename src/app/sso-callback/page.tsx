192aaf806bc6268bfceed544084a86566bfeab1c
ibrahimgermaine1-dev <ibrahimgermaine1@gmail.com>
2026-06-07 16:48:47 +0100

    Delete src directory

--- src/app/sso-callback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthenticateWithRedirectCallback } from '@clerk/nextjs'
-
-/**
- * SSO Callback Page
- * Handles OAuth redirects (Google, etc.) from Clerk.
- */
-export default function SSOCallback() {
-  return <AuthenticateWithRedirectCallback />
-}
